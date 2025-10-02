45d075fc9a2385cfddf783d5aed969329f83116f
Luke_sinclair <Lukesin@outlook.com>
2025-10-02 08:31:59 +0200

    small changes

--- CerealAPI_uge_3/Repositories/SeedData.cs
+++ CerealAPI_uge_3/Repositories/SeedData.cs
@@ -8,20 +8,6 @@ namespace CerealAPI_uge_3.Repositories
 
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            using (var context = new CerealDataContext(
-                   serviceProvider.GetRequiredService<
-                       DbContextOptions<CerealDataContext>>()))
-            {
-                if (!context.cereals.Any())
-                {
-                    using (StreamReader sr = new StreamReader(""))
-                    while (sr.EndOfStream) { }
-
-                }
-            {
-            }
-
-            }
         }
 
     }
